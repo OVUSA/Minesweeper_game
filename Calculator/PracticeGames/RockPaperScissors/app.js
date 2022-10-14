5723a21d51db9aacebe51e9799f415abe6d2c9ef
OVUSA <velichko.olga.chicago@gmail.com>
2022-10-13 20:42:42 -0400

    added user input

--- Calculator/PracticeGames/RockPaperScissors/app.js
+++ Calculator/PracticeGames/RockPaperScissors/app.js
@@ -1,3 +1,11 @@
 const  computerChoice = document.getElementById('computer-choice');
 const userChoice = document.getElementById('user-choice');
 const result = document.getElementById('result');
+const possibleChoices = document.querySelectorAll('button');
+let tempUserChoice;
+
+// passing an event
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click',(e)=>{
+    tempUserChoice = e.target.id
+    userChoice.innerHTML=tempUserChoice;
+}))
