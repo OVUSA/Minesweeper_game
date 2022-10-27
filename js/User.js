517da0fc56518a1c39a272cdf3c22be1137dddd4
OVUSA <velichko.olga.chicago@gmail.com>
2022-10-27 13:20:43 -0400

    updated user class

--- js/User.js
+++ js/User.js
@@ -2,11 +2,15 @@ class User{
 
     constructor(name, password){
         this.name = name;
-        this.password= password;
+        this.password = password;
     }
-    saveUser(){
-        console.log(`${this.name} -user is created`)
-    }
-
 
 }
+const btn = document.getElementById('btn');
+btn.addEventListener('click', () => {
+    var currentUser = new User(
+        document.getElementById('name').value,
+        document.getElementById('password').value);
+        console.log(`Registered user : ${currentUser.name}`);
+
+})
